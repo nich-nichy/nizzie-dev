8d4eaab5827222728aedb195ba10a745e1288599
Mohamed Nishath M <nishathmohamed786@gmail.com>
2024-10-18 11:44:50 +0530

    Created version 1

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/providers/theme-provider'
+import { ClerkProvider } from '@clerk/nextjs'
+import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from '@/components/ui/sonner'
+import { BillingProvider } from '@/providers/billing-provider'
+
+const font = DM_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Nizzie.',
+  description: 'Automate Your Work With Nizzie.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  console.log(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY)
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <BillingProvider>
+              <ModalProvider>
+                {children}
+                <Toaster />
+              </ModalProvider>
+            </BillingProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
